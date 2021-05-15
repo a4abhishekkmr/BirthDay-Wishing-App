517250a6e142abb58b29860c8649a773199a5aae
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2021-05-15 10:28:00 +0530

    Added Toast as a preview
    When clicking the submit button it will show the message with a dialog box

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+    fun createWishingpage(view: View) {
+
+        val name= NameInputt.editableText.toString()
+        Toast.makeText(this,"Creating for you $name", Toast.LENGTH_LONG).show()
+
+    }    }
 }
