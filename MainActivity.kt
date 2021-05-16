a4368f1cdd43e56f72b28a15f75299b81b17e4a9
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2021-05-16 10:16:54 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.birthdaywishingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,10 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-    fun createWishingpage(view: View) {
 
-        val name= NameInputt.editableText.toString()
-        Toast.makeText(this,"Creating for you $name", Toast.LENGTH_LONG).show()
 
-    }    }
+
+        fun callingFunction(view: View) {
+            val name = nameInput.editableText.toString()
+
+            val intent = Intent(this, BirthdayGreetingLayout::class.java)
+            intent.putExtra(BirthdayGreetingLayout.NAME_EXTRA, name)
+            startActivity(intent)
+        }
+    }
 }
