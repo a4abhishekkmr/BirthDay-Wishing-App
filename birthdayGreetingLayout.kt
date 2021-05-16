fa738e98b8b4fb0b3eb00d0aad62037afa28d333
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2021-05-16 10:22:28 +0530

    Update birthdayGreetingLayout.kt

--- birthdayGreetingLayout.kt
+++ birthdayGreetingLayout.kt
@@ -2,10 +2,18 @@ package com.example.birthdaywishingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_birthday_greeting_layout.*
 
 class BirthdayGreetingLayout : AppCompatActivity() {
+
+    companion object{
+        const val NAME_EXTRA ="name_extra"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting_layout)
+
+        val name=intent.getStringExtra(NAME_EXTRA)
+        birthdayGreeting.text="Happy Birthday\n $name"
     }
 }
